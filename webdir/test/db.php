bba0725d1a06685de56b1db94d4f5d1b6ef3f39b
PlexSheep <PlexSheep@protonmail.com>
2023-04-12 16:17:31 +0200

    db working query

--- webdir/test/db.php
+++ webdir/test/db.php
@@ -1,15 +1,35 @@
 <?php
-$servername = "127.0.0.1";
+/*
+ * Working php script, reads all entries of a test table
+ *
+ *
+ * this file runs in a docker container, 127.0.0.1 is the dockercontainer itself.
+ */
+$servername = "db:3306";
 $username = "test";
 $password = "test";
-$port = 3306;
+$dbname = "test_db";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, "test_db", $port);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+echo "Connected successfully<br><br>";
+
+$sql = "SELECT id, bar, qux FROM foo";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - bar: " . $row["bar"]. " - qux: " . $row["qux"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+
 ?>
