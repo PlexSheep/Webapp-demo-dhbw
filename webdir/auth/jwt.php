69db538ad528e5d83babfc9248cda0e3b3a95d89
0xalivecow <10931446-0xalivecow@users.noreply.gitlab.com>
2023-04-14 22:29:29 +0200

    JWT dependency

---
+++ webdir/auth/jwt.php
@@ -0,0 +1,16 @@
+<?php
+require '../vendor/autoload.php';
+
+use Firebase\JWT\JWT;
+
+$payload = array(
+    "iss" => "example.com",
+    "aud" => "example.com",
+    "iat" => 1356999524,
+    "nbf" => 1357000000
+);
+
+$jwt = JWT::encode($payload, "secret", 'HS256');
+
+print_r($jwt)
+?>
