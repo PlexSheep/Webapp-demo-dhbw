54b977917c4fe9bdce81694e0e2e14d66a9691a9
PlexSheep <software@cscherr.de>
2023-04-15 13:40:10 +0200

    added search form to index

--- webdir/search.php
+++ webdir/search.php
@@ -28,12 +28,15 @@ require 'common.php';
     <main>
         <section class="hero">
             <h1>Entdecke Rezepte aus der ganzen Welt und erlebe eine globale Küchenreise</h1>
-            <input type="search" placeholder="Suche nach Rezepten, Zutaten oder Ländern">
+            <form action="search.php" method="get">
+                <input type="search" name="search" id="search" placeholder="Suche nach Rezepten, Zutaten oder Ländern">
+                <input type="submit" value="Los">
+            </form>
         </section>
-        <section class="recipe-detail-container">
-            <center>
-                <p>nothing here yet</p>
-            </center>
+        <section class="featured-recipes">
+            <h2>Beliebte Rezepte</h2>
+            <div class="recipe-grid">
+            </div>
         </section>
     </main>
     <footer>
@@ -42,5 +45,61 @@ require 'common.php';
             <p>&copy; 2023 Globale Küchenreise. Alle Rechte vorbehalten.</p>
         </div>
     </footer>
+    <script>
+      const recipes = [
+    <?php
+        $conn = new DatabaseConnection($ini_array);
+        $result = $conn->query_database("SELECT * FROM recipies");
+        $rows = $result->fetch_all(MYSQLI_ASSOC);
+        foreach ($rows as $row) {
+            echo "\t\t{
+              title: '" . $row['title'] ."',
+              country: '" . $conn->get_country_by_id($row['country'])['name'] . "',
+              imgUrl: 'img/useruploads/" . $row['image_path'] ."',
+              description: '" . $row['description'] . "',
+              id: '" . $row['id'] . "',
+              slug: '" . $row['slug'] . "',
+              score: '" . $row['score'] . "',
+        },\n";
+        }
+
+    ?>
+      ];
+
+      function displayRecipes() {
+        const recipeGrid = document.querySelector('.recipe-grid');
+        
+        recipes.forEach(recipe => {
+          const recipeLink = document.createElement('a');
+          recipeLink.href = "detail.php?recipe=" + recipe.slug;
+          const recipeCard = document.createElement('div');
+          recipeCard.classList.add('recipe-card');
+          
+          const recipeImg = document.createElement('img');
+          recipeImg.src = recipe.imgUrl;
+          recipeImg.alt = recipe.title;
+          recipeCard.appendChild(recipeImg);
+          
+          const recipeTitle = document.createElement('h3');
+          recipeTitle.innerText = recipe.title;
+          recipeCard.appendChild(recipeTitle);
+          
+          const recipeCountry = document.createElement('p');
+          recipeCountry.innerText = `Land: ${recipe.country}`;
+          recipeCard.appendChild(recipeCountry);
+          
+          const recipeDescription = document.createElement('p');
+          recipeDescription.innerText = recipe.description;
+          recipeCard.appendChild(recipeDescription);
+          
+          recipeLink.appendChild(recipeCard);
+          recipeGrid.appendChild(recipeLink);
+        });
+      }
+
+      displayRecipes();
+    </script>
+    
+    
 </body>
 </html>
