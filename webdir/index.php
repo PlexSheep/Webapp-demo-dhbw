fcab6838aa6615ab91b9f5cd7973fc48be721c95
PlexSheep <software@cscherr.de>
2023-04-15 13:56:33 +0200

    early search functionality

--- webdir/index.php
+++ webdir/index.php
@@ -99,7 +99,5 @@ require 'common.php';
 
       displayRecipes();
     </script>
-    
-    
 </body>
 </html>
--- webdir/search.php
+++ webdir/search.php
@@ -33,6 +33,75 @@ require 'common.php';
                 <input type="submit" value="Los">
             </form>
         </section>
+        <section class="search-results">
+    <?php
+        $conn = new DatabaseConnection($ini_array);
+        // TODO FIXME PROBABLY VULNERABLE TO SQL INJECTION
+        if ($_GET['search'] == "") {
+            $query = "SELECT * FROM recipies";  
+        }
+        else {
+            $query = "SELECT * FROM recipies WHERE title ='". $_GET['search'] ."'";  
+        }
+        $result = $conn->query_database($query);
+        $rows = $result->fetch_all(MYSQLI_ASSOC);
+        if ($rows) {
+            echo "<script>const recipes = [";
+            foreach ($rows as $row) {
+                echo "\t\t{
+                  title: '" . $row['title'] ."',
+                  country: '" . $conn->get_country_by_id($row['country'])['name'] . "',
+                  imgUrl: 'img/useruploads/" . $row['image_path'] ."',
+                  description: '" . $row['description'] . "',
+                  id: '" . $row['id'] . "',
+                  slug: '" . $row['slug'] . "',
+                  score: '" . $row['score'] . "',
+            },\n";
+            }
+            // TODO load the script from a js file
+            echo "
+      ];
+
+      function displayRecipes() {
+        const recipeGrid = document.querySelector('.recipe-grid');
+        
+        recipes.forEach(recipe => {
+          const recipeLink = document.createElement('a');
+          recipeLink.href = \"detail.php?recipe=\" + recipe.slug;
+          const recipeCard = document.createElement('div');
+          recipeCard.classList.add('recipe-card');
+          
+          const recipeImg = document.createElement('img');
+          recipeImg.src = recipe.imgUrl;
+          recipeImg.alt = recipe.title;
+          recipeCard.appendChild(recipeImg);
+          
+          const recipeTitle = document.createElement('h3');
+          recipeTitle.innerText = recipe.title;
+          recipeCard.appendChild(recipeTitle);
+          
+          const recipeCountry = document.createElement('p');
+          recipeCountry.innerText = `Land: \${recipe.country}`;
+          recipeCard.appendChild(recipeCountry);
+          
+          const recipeDescription = document.createElement('p');
+          recipeDescription.innerText = recipe.description;
+          recipeCard.appendChild(recipeDescription);
+          
+          recipeLink.appendChild(recipeCard);
+          recipeGrid.appendChild(recipeLink);
+        });
+      }
+
+      displayRecipes();
+    </script>
+";
+        }
+        else {
+            echo "<h1>Keine Rezepte für '" . $_GET['search'] . "' gefunden.</h1>";
+        }
+
+    ?>
         <section class="featured-recipes">
             <h2>Beliebte Rezepte</h2>
             <div class="recipe-grid">
@@ -99,7 +168,5 @@ require 'common.php';
 
       displayRecipes();
     </script>
-    
-    
 </body>
 </html>
