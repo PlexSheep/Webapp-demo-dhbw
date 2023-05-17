0abd3a3c29dfe16c950f1473ea5389516ff57af9
PlexSheep <PlexSheep@protonmail.com>
2023-05-17 16:12:33 +0200

    basic create

--- webdir/create.php
+++ webdir/create.php
@@ -1,6 +1,18 @@
 <?php
 // require the common.php stuff
 require 'common.php';
+// process the form if it was sent
+if($_POST) {
+    print_r($_POST);
+    // parse
+    $tags = $_POST['tags'];
+    $ingredient = $_POST['ingredient'];
+    $category = $_POST['category'];
+    
+    //$conn = new DatabaseConnection($ini_array);
+
+    exit;
+}
 ?>
 <!DOCTYPE html>
 <html lang="de">
@@ -27,7 +39,14 @@ require 'common.php';
                 <input type="text" id="name" name="name" placeholder="Name">
                 <label for="desc-box">Beschreibung</label>
                 <textarea name="desc" id="desc-box" placeholder="Beschreibung des Rezepts" rows="20" cols="120"></textarea>
-
+                <label for="tags">Tags:</label>
+                <input type="text" id="tags" placeholder="Tags">
+                <label for="ingredient">Zutaten:</label>
+                <input type="text" id="ingredient" placeholder="Zutaten">
+                <label for="category">Kategorie:</label> 
+                <select>
+                    <option>test</option>
+                </select>
                 <input type="submit" value="Erstellen">
             </form>
         </section>
