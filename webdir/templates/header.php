f8a0fc1394ea0452f352c8b3c401be9401162a27
PlexSheep <software@cscherr.de>
2023-06-12 16:55:27 +0200

    removed a TODO

--- webdir/templates/header.php
+++ webdir/templates/header.php
@@ -14,7 +14,6 @@
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item" ><a class="nav-link" href="create.php">Erstellen</a></li>
                     <li class="nav-item "><a class="nav-link" href="konto.php">Konto</a></li
-                    <!\-\- TODO remove these links for the test environment for the final version! \-\->
                     <?php
                     if(isset($_SESSION['ID'])){
                         echo "<li class=\"nav-item\"><a class=\"nav-link\" href=\"../auth/logout.php\">Logout</a></li>";
@@ -31,4 +30,3 @@
         </div>
 </nav>
 </body>
-
