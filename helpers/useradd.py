adc5980800fdce88872a651c45e6789951a4463f
0xalivecow <0xalivecow@proton.me>
2023-06-12 14:28:26 +0200

    new user insert

--- helpers/useradd.py
+++ helpers/useradd.py
@@ -1,10 +1,14 @@
 import requests
 
 file1 = open("familynames-usa-top1000.txt", 'r')
+sql = open("users.sql", "a")
 names = file1.readlines()
 
+user_string = """
+({i}, '{name}', '$argon2id$v=19$m=65536,t=4,p=1$ZkRjNVJVaU83M1E1U3EuMg$ANBFNE+qN31RzlANFQGVsFoqS1LHqLJ/iNlQxt9p1YA', '{name}@test.de'),
+"""
+
 for i in range(0, 1000):
-    r = requests.post("http://localhost:8080/auth/register.php", data={'MAIL': names[i] + '@test.com', 'USER': names[i], 'PASS':'password'})
-    print(r)
+    sql.write(user_string.format(i = i, name = names[i].strip("\n")))
 
 
