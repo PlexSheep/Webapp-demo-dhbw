cb5bc14b9c8d1cf3a567e7b6b73e4010a4cb9764
PlexSheep <software@cscherr.de>
2023-06-13 19:58:25 +0200

    temp fix for db script

--- dev/docker_mysql_init/Recepie_Service.sql
+++ dev/docker_mysql_init/Recepie_Service.sql
@@ -21,6 +21,10 @@ SET time_zone = "+00:00";
 -- Database: `Recepie_Service`
 --
 
+-- i added this myself, to fix the db being unable to start
+CREATE DATABASE Recepie_Service;
+USE Recepie_Service;
+
 -- --------------------------------------------------------
 
 --
