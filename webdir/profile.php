d6db5b686f5ef01c18e8cf90d2991cf8764c722c
PlexSheep <software@cscherr.de>
2023-06-13 20:30:04 +0200

    profile works

--- webdir/profile.php
+++ webdir/profile.php
@@ -24,46 +24,51 @@ if (!isset($_GET['user'])) {
                         }
 
                         echo '<h4 class="display-4 mt-5 mx-5">Rezepte von ' . $user['username'] . '</h4><br>';
-                        echo '<div class="recipe-grid container-fluid px-5 mt-5" id="featured-recipies-grid">';
 
                         $rows = $conn->query_recipies_user($_GET['user'])->get_result()->fetch_all(MYSQLI_ASSOC);
                         if (!isset($rows) || $rows === NULL) {
                             exit_with_not_found();
                         }
-                        foreach ($rows as $row) {
-                            if ($row['image_path'] === null) {
-                                echo '
-                                <a href=/detail.php?recipie=' . $row['slug'] . '>
-                                <div class="card" style="width: 18rem;">
-                                  <div>
-                                      <img src="img/icons/empty_plate.jpg" class="card-img-top card-img" alt="thumbnail">
-                                  </div>
-                                  <div class="card-body">
-                                    <h5 class="card-title">' . htmlspecialchars($row['title'], ENT_QUOTES, 'UTF-8') . '</h5>
-                                    <p class="card-text">' . htmlspecialchars(escape_newlines($row['description'], ENT_QUOTES, 'UTF-8')) . '</p>
-                                  </div>
-                                  <ul class="list-group list-group-flush">
-                                      <li class="list-group-item">' . $conn->get_country_by_id($row['country'])['name'] . '</li>
-                                  </ul>
-                                </div>
-                                </a>';
-                            }
-                            else {
-                                echo '
-                                <a href=/detail.php?recipie=' . $row['slug'] . '>
-                                <div class="card" style="width: 18rem;">
-                                  <div>
-                                      <img src="img/useruploads/' . $row['image_path'] . '" class="card-img-top card-img" alt="thumbnail">
-                                  </div>
-                                  <div class="card-body">
-                                    <h5 class="card-title">' . htmlspecialchars($row['title'], ENT_QUOTES, 'UTF-8') . '</h5>
-                                    <p class="card-text">' . htmlspecialchars(escape_newlines($row['description'], ENT_QUOTES, 'UTF-8')) . '</p>
-                                  </div>
-                                  <ul class="list-group list-group-flush">
-                                      <li class="list-group-item">' . $conn->get_country_by_id($row['country'])['name'] . '</li>
-                                  </ul>
-                                </div>
-                                </a>';
+                        if (count($rows) < 1) {
+                            echo '<p class="fs-3 mx-5">Dieser Nutzer hat keine Rezepte erstellt.</p>';
+                        }
+                        else {
+                            echo '<div class="recipe-grid container-fluid px-5 mt-5" id="featured-recipies-grid">';
+                            foreach ($rows as $row) {
+                                if ($row['image_path'] === null) {
+                                    echo '
+                                    <a href=/detail.php?recipie=' . $row['slug'] . '>
+                                    <div class="card" style="width: 18rem;">
+                                      <div>
+                                          <img src="img/icons/empty_plate.jpg" class="card-img-top card-img" alt="thumbnail">
+                                      </div>
+                                      <div class="card-body">
+                                        <h5 class="card-title">' . htmlspecialchars($row['title'], ENT_QUOTES, 'UTF-8') . '</h5>
+                                        <p class="card-text">' . htmlspecialchars(escape_newlines($row['description'], ENT_QUOTES, 'UTF-8')) . '</p>
+                                      </div>
+                                      <ul class="list-group list-group-flush">
+                                          <li class="list-group-item">' . $conn->get_country_by_id($row['country'])['name'] . '</li>
+                                      </ul>
+                                    </div>
+                                    </a>';
+                                }
+                                else {
+                                    echo '
+                                    <a href=/detail.php?recipie=' . $row['slug'] . '>
+                                    <div class="card" style="width: 18rem;">
+                                      <div>
+                                          <img src="img/useruploads/' . $row['image_path'] . '" class="card-img-top card-img" alt="thumbnail">
+                                      </div>
+                                      <div class="card-body">
+                                        <h5 class="card-title">' . htmlspecialchars($row['title'], ENT_QUOTES, 'UTF-8') . '</h5>
+                                        <p class="card-text">' . htmlspecialchars(escape_newlines($row['description'], ENT_QUOTES, 'UTF-8')) . '</p>
+                                      </div>
+                                      <ul class="list-group list-group-flush">
+                                          <li class="list-group-item">' . $conn->get_country_by_id($row['country'])['name'] . '</li>
+                                      </ul>
+                                    </div>
+                                    </a>';
+                                }
                             }
                         }
 
